00787a462b8e61336e472d5bde3bb79e09a41fd0
Rahul B U <rahulbu83@gmail.com>
2018-10-24 17:08:09 +0000

    menu bar

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,20 @@ const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   password: 'rahul',
-//   database:'DATABASE',
+  database:'mydb',
   ssl: true,
 });
 
 client.connect();
 
-router.get("/psql",(req,res)=>{
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, result) => {
-  if (err) throw err;
-    res.render("index",{res : result});
+// router.get("/psql",(req,res)=>{
+//     client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, result) => {
+//   if (err) throw err;
+//     res.render("index",{res : result});
 
-});
-})
-// client.query('SELECT * from demo', (err, res) => {
+// });
+// })
+// // client.query('SELECT * from demo', (err, res) => {
 //   if (err) throw err;
 //   for (let row of res.rows) {
 //     console.log(JSON.stringify(row));
@@ -33,10 +33,40 @@ router.get("/",(req,res)=>{
 //   for (let row of res.rows) {
 //     console.log(JSON.stringify(row));
 //   }
-  client.end();
+  //client.end();
 });
     // res.render("index");
 })
 
+
+/////////////////////////////////////////////////////////////////////////////////
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+
+// client.connect();
+// router.get("/",function(req,res){
+//     client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
+// })
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+router.get("/login",(req,res)=>{
+  res.render("login");
+})
+
+router.get("/register",(req,res)=>{
+  res.render("register");
+})
 
 module.exports = router;
