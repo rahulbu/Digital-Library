bef603635ba9a5b016629f88aa2871b6ad25d82c
Rahul B U <rahulbu83@gmail.com>
2018-11-23 09:52:16 +0000

    flash message

--- routes/report.js
+++ routes/report.js
@@ -7,37 +7,53 @@ const router = require("express").Router(),
       
 router.use(fileUpload());
 
-router.get("/:pid",(req,res)=>{
+router.get("/:pid",middleware.isLoggedIn,(req,res)=>{
     
     res.sendFile(path.join(__dirname,"/../reports/"+req.params.pid + "report.pdf"));
 })
 
-router.post("/:pid",(req,res)=>{
+router.post("/:pid",middleware.checkStudent,(req,res)=>{
     const file = req.files.file;
     console.log(file.name);
     file.mv(path.join(__dirname,"/../reports",req.params.pid + "report.pdf"),(error)=>{
-        if(error) console.log(error);
-        else console.log("file uploaded");
+        if(error) {
+            console.log(error);
+            req.flash("error","File upload failed !");
+            res.redirect("back");
+        }
+        else {
+            req.flash("success","Report uploaded successfully !")
+            // console.log("File uploaded");
+            res.redirect("back");
+        }
     })
     const reportPath = "/reports/"+req.params.pid + "report.pdf";
     db.query("update project set report=$1 where pid = $2",[reportPath,req.params.pid],(err,result)=>{
         if(err) console.log(err);
         else console.log("report updated in db")
     })
-    res.redirect("back");
+    // res.redirect("/projects/"+req.params.pid);
 })
 
-router.get("/:pid/delete",(req,res)=>{
+router.get("/:pid/delete",middleware.checkStudent,(req,res)=>{
     upload.delete(req.params.pid+'report.pdf',(error)=>{
-        if(error) console.log (error);
-        else console.log("file deleted")
+        if(error) {
+            req.flash("error","error !!")
+            console.log (error);
+            res.redirect("back");
+        }
+        else {
+            console.log("file deleted")
+            req.flash("success","Report removed successfully !")
+            res.redirect("back");
+        }
         // res.redirect("/");
     })
     db.query("update project set report=$1 where pid = $2",["",req.params.pid],(err,result)=>{
         if(err) console.log(err);
         else console.log("report updated in db")
     })
-    res.redirect("back");
+    // res.redirect("/projects/"+req.params.pid);
 })
 
 
